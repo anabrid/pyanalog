0319a1b14659427a5df7e59bc7dad8f560bbd76d
Sven Sveto <sven@sveto>
2020-04-30 14:10:56 +0200

    Adding testbench.

---
+++ 20200430-testing-stability/sven_20200429.pl
@@ -0,0 +1,18 @@
+use strict;
+use warnings;
+
+BEGIN {push @INC, '../HybridController/IO-HyCon/lib/'}
+
+use IO::HyCon;
+use POSIX;
+
+my $ac = IO::HyCon->new();
+$ac->setup('XBAR16');
+$ac->single_run_sync();
+$ac->get_data();
+$ac->plot(terminal => "pdf", output => "output.pdf",
+    title => strftime("%Y-%m-%d %H:%M:%S", localtime time),
+    lines => [
+    	"set yrange [-1:1]"    
+    ]);
+$ac->store_data(filename => 'result.dat');
