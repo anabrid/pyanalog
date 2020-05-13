34e9944b5d28389e8d48ccacd5592044b9877409
SvenK <sven+git.ambrosius@svenk.org>
2020-05-13 12:26:37 +0200

    Comparing exp creations

--- 20200430-testing-stability/plot.py
+++ 20200430-testing-stability/plot.py
@@ -1,14 +1,32 @@
 #!/usr/bin/env python3
 
 from pylab import *
-ion()
+clf(); ion()
 
-data = genfromtxt("result.dat", loose=True, invalid_raise=False)
-x,z,t = data.T
+data = genfromtxt("y_t_eq_y_and_time.dat", loose=True, invalid_raise=False)
+yt,t = data.T
+
+data = genfromtxt("y_tt.dat", loose=True, invalid_raise=False)
+ytt = data[:,0]
+
+data = genfromtxt("y_ttt.dat", loose=True, invalid_raise=False)
+yttt = data[:,0]
 
 # "fit" time
-dt = average(diff(t)[:30]
-tp = arange(len(x))*dt
+dt = abs(average(diff(t)[:15]))
+t = lambda y: arange(len(y))*dt
 
-plot(tp, x)
-plot(tp, z)
+exact = exp(-t(yt))
+
+plot(t(yt), yt, label="$y' = -y$")
+plot(t(ytt), ytt, label="$y'' = +y$")
+plot(t(yttt), yttt, label="$y''' = -y$")
+plot(t(exact), exact, "--", label="Exact solution")
+
+legend()
+
+xlim(0,20)
+title("Three ways of producing exp(-t) on an analog computer...")
+
+tight_layout()
+
