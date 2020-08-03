a487128e8725f6395ba4fcaa61c2955d360b2f3f
Sven Sveto <sven@sveto>
2020-08-03 04:29:35 +0200

    Chua als nächstes Beispiel hinzugefügt

---
+++ examples/run-chua.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+
+import os
+
+def sh(cmd):
+    print(cmd)
+    if os.system(cmd): raise ValueError(f"FAILED: {cmd}")
+
+#base = "unscaled_chua"
+base = "chua"
+
+sh(f"python -m dda  {base}.dda C > {base}.cpp")
+sh(f"g++ --std=c++20 -o {base}.out {base}.cpp")
+sh(f"./{base}.out --max_iterations=100000 --modulo_write=10 > data.out")
+
+from pylab import *
+
+data = genfromtxt("data.out", names=True)
+
+x,y,z = data["x"], data["y"], data["z"]
+
+ion()
+
+plot(x,y,lw=0.7)
+
+## alternatively, we can plot something which
+## alternates color by the z value:
+# scatter(x,y,c=z)
