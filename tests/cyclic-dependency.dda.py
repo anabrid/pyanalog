337b7f8571a7848198b832a8d3d119736142a542
Sven Sveto <sven@sveto>
2020-09-10 13:04:11 +0200

    Updating license on all files.
    The difference to the comment before is just one call to ./LICENSE.py

--- tests/cyclic-dependency.dda.py
+++ tests/cyclic-dependency.dda.py
@@ -6,6 +6,21 @@
 # This file is part of the test suite of the PyAnalog toolkit.
 #
 # ANABRID_BEGIN_LICENSE:GPL
+# Commercial License Usage
+# Licensees holding valid commercial anabrid licenses may use this file in
+# accordance with the commercial license agreement provided with the
+# Software or, alternatively, in accordance with the terms contained in
+# a written agreement between you and Anabrid GmbH. For licensing terms
+# and conditions see https://www.anabrid.com/licensing. For further
+# information use the contact form at https://www.anabrid.com/contact.
+# 
+# GNU General Public License Usage
+# Alternatively, this file may be used under the terms of the GNU 
+# General Public License version 3 as published by the Free Software
+# Foundation and appearing in the file LICENSE.GPL3 included in the
+# packaging of this file. Please review the following information to
+# ensure the GNU General Public License version 3 requirements
+# will be met: https://www.gnu.org/licenses/gpl-3.0.html.
 # ANABRID_END_LICENSE
 #
 
